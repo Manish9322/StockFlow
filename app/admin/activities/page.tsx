d9a0f64963aa628d4f70e5850f64b18a86d1ae3f
Manish9322 <manishsonawane3010@gmail.com>
2025-12-25 20:16:58 +0530

    Imp : Changed the default row count on table on activities page

--- app/admin/activities/page.tsx
+++ app/admin/activities/page.tsx
@@ -70,7 +70,7 @@ function AdminActivitiesContent() {
   
   // Pagination state
   const [currentPage, setCurrentPage] = useState(1)
-  const [itemsPerPage, setItemsPerPage] = useState(10)
+  const [itemsPerPage, setItemsPerPage] = useState(5)
 
   useEffect(() => {
     fetchMovements()
